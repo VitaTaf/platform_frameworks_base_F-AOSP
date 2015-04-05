2869666ff70d7d8d97cea9ea957b1428041b8654
Wale Ogunwale <ogunwale@google.com>
2015-04-04 17:10:50 -0700

    Start non-running activity that should be visible in resume state.
    In multi-window mode it is possible for the system-ui launch
    multiple activities at the same time that might not all be in a
    running process. For the activities that are not in a running
    process to be visible, they need to start in the resume state
    not the stopped state if they will be the only resumed activity
    in the stack.

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -1261,6 +1261,7 @@ final class ActivityStack {
         // make sure any activities under it are now visible.
         boolean aboveTop = true;
         boolean behindFullscreen = !isStackVisibleLocked();
+        boolean noStackActivityResumed = (isInStackLocked(starting) == null);
 
         for (int taskNdx = mTaskHistory.size() - 1; taskNdx >= 0; --taskNdx) {
             final TaskRecord task = mTaskHistory.get(taskNdx);
@@ -1291,9 +1292,8 @@ final class ActivityStack {
                     }
 
                     if (r.app == null || r.app.thread == null) {
-                        // This activity needs to be visible, but isn't even
-                        // running...  get it started, but don't resume it
-                        // at this point.
+                        // This activity needs to be visible, but isn't even running...
+                        // get it started and resume if no other stack in this stack is resumed.
                         if (DEBUG_VISBILITY) Slog.v(TAG, "Start and freeze screen for " + r);
                         if (r != starting) {
                             r.startFreezingScreenLocked(r.app, configChanges);
@@ -1304,7 +1304,9 @@ final class ActivityStack {
                             setVisibile(r, true);
                         }
                         if (r != starting) {
-                            mStackSupervisor.startSpecificActivityLocked(r, false, false);
+                            mStackSupervisor.startSpecificActivityLocked(
+                                    r, noStackActivityResumed, false);
+                            noStackActivityResumed = false;
                             if (activityNdx >= activities.size()) {
                                 // Record may be removed if its process needs to restart.
                                 activityNdx = activities.size() - 1;
