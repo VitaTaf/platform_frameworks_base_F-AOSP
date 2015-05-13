47ec5a9224bcafc318b68bf94a1284c417a30bbc
Wale Ogunwale <ogunwale@google.com>
2015-05-13 15:27:12 -0700

    Don't stop animation when starting windows app token changes
    If another activity is starting while we are still animating
    the starting window of the previous activity to start, we
    transfer the starting window of the old activity to the one that
    is currently starting so we don't have to create another starting
    window and also to reduce jank from the starting window animation
    appearing to restart.
    
    However, there were several conditions that led to the starting
    window animation stopping when the transfers app tokens
    
    1. Starting window animator not been removed from the previous
    app token animator causing it to finish/remove the starting window
    prematurily.
    2. Starting window animator not been properly added to the new
    app token animator causing the animation not be be picked up.
    3. WMS.mSkipAppTransitionAnimation been set to false regardless of if
    an app transition was actually prepared in WMS.prepareAppTransition()
    4. WMS.mSkipAppTransitionAnimation not been set to true in all cases
    where the starting window transfers tokens even though we don't want the
    new app to do any transition animation is the starting window is
    animating.
    5. New app not setting its animation to dummy animation when the next
    transition should be skipped due to starting window still animating.
    6. Starting window animation been cleared for the new app in
    WMS.handleAppTransitionReadyLocked() even for cases where we transferred
    the animation from the previous app.
    
    Also, cleaned up some code.
    
    Bug: 20953232
    Change-Id: I714d6bdfcdaeeaac29f9d464bab9f3e9c192e937

--- services/core/java/com/android/server/wm/AppTransition.java
+++ services/core/java/com/android/server/wm/AppTransition.java
@@ -289,11 +289,13 @@ public class AppTransition implements Dump {
         return mNextAppTransitionStartY;
     }
 
-    void prepare() {
+    boolean prepare() {
         if (!isRunning()) {
             mAppTransitionState = APP_STATE_IDLE;
             notifyAppTransitionPendingLocked();
+            return true;
         }
+        return false;
     }
 
     void goodToGo(AppWindowAnimator openingAppAnimator, AppWindowAnimator closingAppAnimator) {
@@ -954,8 +956,8 @@ public class AppTransition implements Dump {
                     : com.android.internal.R.anim.voice_activity_open_exit);
             if (DEBUG_APP_TRANSITIONS || DEBUG_ANIM) Slog.v(TAG,
                     "applyAnimation voice:"
-                    + " anim=" + a + " transit=" + transit + " isEntrance=" + enter
-                    + " Callers=" + Debug.getCallers(3));
+                    + " anim=" + a + " transit=" + appTransitionToString(transit)
+                    + " isEntrance=" + enter + " Callers=" + Debug.getCallers(3));
         } else if (isVoiceInteraction && (transit == TRANSIT_ACTIVITY_CLOSE
                 || transit == TRANSIT_TASK_CLOSE
                 || transit == TRANSIT_TASK_TO_BACK)) {
@@ -964,22 +966,23 @@ public class AppTransition implements Dump {
                     : com.android.internal.R.anim.voice_activity_close_exit);
             if (DEBUG_APP_TRANSITIONS || DEBUG_ANIM) Slog.v(TAG,
                     "applyAnimation voice:"
-                    + " anim=" + a + " transit=" + transit + " isEntrance=" + enter
-                    + " Callers=" + Debug.getCallers(3));
+                    + " anim=" + a + " transit=" + appTransitionToString(transit)
+                    + " isEntrance=" + enter + " Callers=" + Debug.getCallers(3));
         } else if (mNextAppTransitionType == NEXT_TRANSIT_TYPE_CUSTOM) {
             a = loadAnimationRes(mNextAppTransitionPackage, enter ?
                     mNextAppTransitionEnter : mNextAppTransitionExit);
             if (DEBUG_APP_TRANSITIONS || DEBUG_ANIM) Slog.v(TAG,
                     "applyAnimation:"
                     + " anim=" + a + " nextAppTransition=ANIM_CUSTOM"
-                    + " transit=" + transit + " isEntrance=" + enter
+                    + " transit=" + appTransitionToString(transit) + " isEntrance=" + enter
                     + " Callers=" + Debug.getCallers(3));
         } else if (mNextAppTransitionType == NEXT_TRANSIT_TYPE_CUSTOM_IN_PLACE) {
             a = loadAnimationRes(mNextAppTransitionPackage, mNextAppTransitionInPlace);
             if (DEBUG_APP_TRANSITIONS || DEBUG_ANIM) Slog.v(TAG,
                     "applyAnimation:"
-                            + " anim=" + a + " nextAppTransition=ANIM_CUSTOM_IN_PLACE"
-                            + " transit=" + transit + " Callers=" + Debug.getCallers(3));
+                    + " anim=" + a + " nextAppTransition=ANIM_CUSTOM_IN_PLACE"
+                    + " transit=" + appTransitionToString(transit)
+                    + " Callers=" + Debug.getCallers(3));
         } else if (mNextAppTransitionType == NEXT_TRANSIT_TYPE_CLIP_REVEAL) {
             a = createClipRevealAnimationLocked(transit, enter, appFrame);
             if (DEBUG_APP_TRANSITIONS || DEBUG_ANIM) Slog.v(TAG,
@@ -991,7 +994,7 @@ public class AppTransition implements Dump {
             if (DEBUG_APP_TRANSITIONS || DEBUG_ANIM) Slog.v(TAG,
                     "applyAnimation:"
                     + " anim=" + a + " nextAppTransition=ANIM_SCALE_UP"
-                    + " transit=" + transit + " isEntrance=" + enter
+                    + " transit=" + appTransitionToString(transit) + " isEntrance=" + enter
                     + " Callers=" + Debug.getCallers(3));
         } else if (mNextAppTransitionType == NEXT_TRANSIT_TYPE_THUMBNAIL_SCALE_UP ||
                 mNextAppTransitionType == NEXT_TRANSIT_TYPE_THUMBNAIL_SCALE_DOWN) {
@@ -1004,7 +1007,7 @@ public class AppTransition implements Dump {
                         "ANIM_THUMBNAIL_SCALE_UP" : "ANIM_THUMBNAIL_SCALE_DOWN";
                 Slog.v(TAG, "applyAnimation:"
                         + " anim=" + a + " nextAppTransition=" + animName
-                        + " transit=" + transit + " isEntrance=" + enter
+                        + " transit=" + appTransitionToString(transit) + " isEntrance=" + enter
                         + " Callers=" + Debug.getCallers(3));
             }
         } else if (mNextAppTransitionType == NEXT_TRANSIT_TYPE_THUMBNAIL_ASPECT_SCALE_UP ||
@@ -1019,7 +1022,7 @@ public class AppTransition implements Dump {
                         "ANIM_THUMBNAIL_ASPECT_SCALE_UP" : "ANIM_THUMBNAIL_ASPECT_SCALE_DOWN";
                 Slog.v(TAG, "applyAnimation:"
                         + " anim=" + a + " nextAppTransition=" + animName
-                        + " transit=" + transit + " isEntrance=" + enter
+                        + " transit=" + appTransitionToString(transit) + " isEntrance=" + enter
                         + " Callers=" + Debug.getCallers(3));
             }
         } else {
@@ -1085,7 +1088,7 @@ public class AppTransition implements Dump {
                     "applyAnimation:"
                     + " anim=" + a
                     + " animAttr=0x" + Integer.toHexString(animAttr)
-                    + " transit=" + transit + " isEntrance=" + enter
+                    + " transit=" + appTransitionToString(transit) + " isEntrance=" + enter
                     + " Callers=" + Debug.getCallers(3));
         }
         return a;
@@ -1189,7 +1192,7 @@ public class AppTransition implements Dump {
 
     @Override
     public String toString() {
-        return "mNextAppTransition=0x" + Integer.toHexString(mNextAppTransition);
+        return "mNextAppTransition=" + appTransitionToString(mNextAppTransition);
     }
 
     /**
