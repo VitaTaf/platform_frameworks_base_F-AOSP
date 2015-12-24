9ad21b4adc4b175820b8345bd70b80b207c813f4
VitaTaf <BryanTan0204@Gmail.com>
2015-12-23 21:59:42 -0800

    Revert "systemui: Handle case when network has been lost"
    This reverts commit b2c763684e968bbb8458d90363a4988b513569aa.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/SecurityControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/SecurityControllerImpl.java
@@ -190,8 +190,7 @@ public class SecurityControllerImpl implements SecurityController {
             NetworkCapabilities networkCapabilities =
                     mConnectivityManager.getNetworkCapabilities(network);
             if (DEBUG) Log.d(TAG, "onAvailable " + network.netId + " : " + networkCapabilities);
-            if (networkCapabilities != null &&
-                    networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)) {
+            if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)) {
                 setCurrentNetid(network.netId);
             }
         };
