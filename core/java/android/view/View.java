91fbd4052f784d17ef93282313c2115a49fa0e23
VitaTaf <BryanTan0204@Gmail.com>
2015-12-26 22:03:53 -0800

    Ensure scroll drawables are managed correctly

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -4509,6 +4509,8 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
 
         if (scrollabilityCache.scrollBar == null) {
             scrollabilityCache.scrollBar = new ScrollBarDrawable();
+            scrollabilityCache.scrollBar.setCallback(this);
+            scrollabilityCache.scrollBar.setState(getDrawableState());
         }
 
         final boolean fadeScrollbars = a.getBoolean(R.styleable.View_fadeScrollbars, true);
@@ -11653,6 +11655,8 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
 
         if (scrollCache.scrollBar == null) {
             scrollCache.scrollBar = new ScrollBarDrawable();
+            scrollCache.scrollBar.setCallback(this);
+            scrollCache.scrollBar.setState(getDrawableState());
         }
 
         if (isHorizontalScrollBarEnabled() || isVerticalScrollBarEnabled()) {
@@ -15987,7 +15991,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
      * @see #drawableStateChanged()
      */
     protected boolean verifyDrawable(Drawable who) {
-        return who == mBackground;
+        return who == mBackground || (mScrollCache != null && mScrollCache.scrollBar == who);
     }
 
     /**
@@ -16002,13 +16006,22 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
      * @see Drawable#setState(int[])
      */
     protected void drawableStateChanged() {
+        final int[] state = getDrawableState();
+
         final Drawable d = mBackground;
         if (d != null && d.isStateful()) {
-            d.setState(getDrawableState());
+            d.setState(state);
+        }
+
+        if (mScrollCache != null) {
+            final Drawable scrollBar = mScrollCache.scrollBar;
+            if (scrollBar != null && scrollBar.isStateful()) {
+                scrollBar.setState(state);
+            }
         }
 
         if (mStateListAnimator != null) {
-            mStateListAnimator.setState(getDrawableState());
+            mStateListAnimator.setState(state);
         }
     }
 
