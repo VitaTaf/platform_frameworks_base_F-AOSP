8fd7ee74c39ae1cb8741a585dbb913215d2be565
VitaTaf <BryanTan0204@Gmail.com>
2016-01-18 17:45:19 -0800

    Fixes

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanelComponent.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanelComponent.java
@@ -60,8 +60,8 @@ public class VolumePanelComponent implements VolumeComponent {
         mSysui = sysui;
         mContext = context;
         mHandler = handler;
-        mAudioManager = context.getSystemService(AudioManager.class);
-        mMediaSessionManager = context.getSystemService(MediaSessionManager.class);
+        mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        mMediaSessionManager = (MediaSessionManager) context.getSystemService(Context.MEDIA_SESSION_SERVICE);
         mVolumeController = new VolumeController();
         mRemoteVolumeController = new RemoteVolumeController();
         mDismissDelay = mContext.getResources().getInteger(R.integer.volume_panel_dismiss_delay);
